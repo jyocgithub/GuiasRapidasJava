aa76b7c290c201308e61d300b172b9f33a57fb7d
javayotrascosas <javayotrascosas@gmail.com>
2020-04-30 15:54:52 +0200

    Actualizacion 3

--- src/GUIA_01_TIPOSDEDATOS.java
+++ src/GUIA_01_TIPOSDEDATOS.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class GUIA_01_TIPOSDEDATOS {
 
 
@@ -28,6 +31,128 @@ public class GUIA_01_TIPOSDEDATOS {
         // y este es el truqui que todos los mortales usamos
         String cade2 = num+"";
 
+
+
+        // ================================================
+        // CASTING - CONVERSION ENTRE TIPOS NUMERICOS
+        // ================================================
+        int i = 23;
+        double d = 34.89;
+
+        d = i;  // se hace un casting implicitio, directamente por el traductor
+        i  = (int) d;  // se necesita hacer un casting explicitio. i vale ahora 34 (no redondea)
+
+
+
+        // =======================================
+        // SALIDA POR CONSOLA
+        // =======================================
+
+        System.out.println("Mensaje que acaba en un salto de linea ");
+        System.out.print("Mensaje que no caba en un salto de linea ");
+        System.out.print("Mensaje que si acaba en un salto de linea \n");
+
+
+
+        // =======================================
+        // ENTRADA POR TECLADO
+        // =======================================
+
+        Scanner entrada = new Scanner(System.in);
+        String linea = entrada.nextLine();
+        int numentero = entrada.nextInt();
+        double numdouble = entrada.nextDouble();
+        // Se recomienda usar un Scanner distinto para numeros y para Strings
+
+
+        // =======================================
+        // RANDOM  _ ELEGIR NUMEROS AL AZAR
+        // =======================================
+
+        /*
+         * CON MATH.random()
+         */
+
+        int a = (int) (Math.random () * 10);  // de 0 a 9
+        int b = (int) (Math.random () * (100-50+1)+50);  // entre 51 y 100
+
+        /*
+         * CON RANDOM
+         */
+
+        Random azar = new Random();
+        int va1 = azar.nextInt(10);             // de 0 a 9
+        int va2 = azar.nextInt(100-50+1) + 50;  // de 50 a 100
+
+
+        // =======================================
+        // EJEMPLO : pide una edad y muestra un mensaje con dicha edad
+        // =======================================
+
+        // Se crea una variable llamada (por ejemplo) input, que es como una maquina de leer cosas por la consola
+        Scanner input = new Scanner(System.in);
+        // Ponemos un mensaje al usuario para que vea lo que tiene que escribir
+        System.out.println("dame tu edad ");
+        // Llamamos a la "maquina de leer" (input) y le pedimos que lea un int (nextInt() )
+        // El valor que escribimos en el teclado va a parar a la variable 'edad'
+        int edad = input.nextInt();
+        // Escribimos un mensaje con la edad que acabamos de escribir
+        System.out.println("anda pues tienes " + edad + " años");
+        // al acabar el programa, cerramos (usando close() ) la maquina de leer por teclado
+        input.close();
+
+
+        // =======================================
+        // EJEMPLO :  pide dos números y muestra su suma
+        // =======================================
+        // Se crea una variable llamada input2, que es como una maquina de leer cosas por la consola
+        Scanner input2 = new Scanner(System.in);
+        // Ponemos un mensaje al usuario para que vea lo que tiene que escribir
+        System.out.println("dame un primer numero");
+        // Llamamos a la "maquina de leer" (input) y le pedimos que lea un int (nextInt() )
+        // El valor que escribimos en el teclado va a parar a la variable 'valor1'
+        int valor1 = input2.nextInt();
+        // Ponemos un mensaje al usuario para que vea lo que tiene que escribir
+        System.out.println("dame un segundo numero");
+        // Llamamos a la "maquina de leer" (input) y le pedimos que lea un int (nextInt() )
+        // El valor que escribimos en el teclado va a parar a la variable 'valor2'
+        int valor2 = input2.nextInt();
+        // Una vez que tenemos los dos valores , los sumamos y guardamos en otra variable (resultado)
+        int resultado = valor1 + valor2;
+        // Escribimos un mensaje con el resultado de la suma (que es la varible resultado)
+        System.out.println("El resultado de la suma es " + resultado + " y ya hemos acabado");
+        // al acabar el programa, cerramos (usando close() ) la maquina de leer por teclado
+        input2.close();
+
+
+
+        // =======================================
+        // EJEMPLO : convertir kilograms en pounds
+        // =======================================
+
+        // Se crea una variable llamada input3, que es como una maquina de leer cosas por la consola
+        Scanner input3 = new Scanner(System.in);
+
+        // Se crea una CONSTANTE llamada POUNDS_IN_KILOGRAM que indica cuantos pounds hay en un kilogram
+        final double POUNDS_POR_KILO = 2.204620;
+
+        // Ponemos un mensaje al usuario para que vea lo que tiene que escribir
+        System.out.println("Dime una cantidad de kilos :");
+
+        // Llamamos a la "maquina de leer" (input) y le pedimos que lea un double (nextDouble() )
+        // El valor que escribimos en el teclado va a parar a la variable 'numberOfKg'
+        double numberOfKg = input3.nextDouble();
+
+        // Calculamos cuantas pounds hay en los kilos pedidos antes
+        // y el resulado lo guardamos en la variable numberOfPounds
+        double numberOfPounds = numberOfKg * POUNDS_POR_KILO;
+
+        // Escribimos un mensaje con el resultado de la conversion (que es la varible numberOfPounds)
+        // Y añadimos algo de texto, para dar mas informacion.
+        System.out.println(numberOfKg + " kg son " + numberOfPounds + " pounds");
+
+        // al acabar el programa, cerramos (usando close() ) la maquina de leer por teclado
+        input.close();
 
 
 
