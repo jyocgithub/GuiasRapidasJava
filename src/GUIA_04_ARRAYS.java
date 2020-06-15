31782245991a9bcdfb37c07f4f25563082224e5e
javayotrascosas <javayotrascosas@gmail.com>
2020-06-15 11:47:48 +0200

    Actualizacion continua

--- src/GUIA_04_ARRAYS.java
+++ src/GUIA_04_ARRAYS.java
@@ -168,18 +168,18 @@ public class GUIA_04_ARRAYS {
 
 
         /*
-         * EJERCICIO: Ordenar una matriz por metodo de burbuja
+         * EJERCICIO: Ordenar todos los elementos de una matriz por metodo de burbuja
          */
         int[][]  mat = {{14,7,23,45},{33,2,11,4},{8,1,99,45}};
         int n = mat.length;
         int m = mat[0].length;
         int t;
 
-        for( int i=0; i < n; i++){  //ordena la matriz de arriba hacia abajo
+        for( int i=0; i < n; i++){
             for( int j=0;j< m; j++){
                 for(int x=0; x < n; x++){
                     for(int y=0; y <m; y++){
-                        if(mat[i][j] < mat[x][y]){
+                        if(mat[i][j] < mat[x][y]){    // ordena la matriz de menor a mayor
                             t = mat[i][j];
                             mat[i][j] = mat[x][y];
                             mat[x][y] = t;
@@ -187,6 +187,13 @@ public class GUIA_04_ARRAYS {
                     }
                 }
             }
+        }
+        // pintamos la matriz para ver si se ha ordenado
+        for( int i=0; i < n; i++){// pinta la matriz
+            for( int j=0;j< m; j++){
+                System.out.print(mat[i][j]+"  ");
+            }
+            System.out.println();
         }
 
     }
